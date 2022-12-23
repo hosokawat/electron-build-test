efdd167a3e3efcfea412f448d15ff881d99ee8f6
hosokawa.takahiro <vwxyzzy@gmail.com>
2022-12-24 08:47:31 +0900

    publisher-github のtest

--- forge.config.js
+++ forge.config.js
@@ -1,4 +1,17 @@
 module.exports = {
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'hosokawat',
+          name: 'electron-build-test',
+        },
+        prerelease: false,
+        draft: true,
+      },
+    },
+  ],
   packagerConfig: {},
   rebuildConfig: {},
   makers: [
