76d0cf797c4d25514b85234ba10955751e136b2f
hosokawa.takahiro <vwxyzzy@gmail.com>
2022-12-24 11:32:19 +0900

    test

--- forge.config.js
+++ forge.config.js
@@ -12,7 +12,15 @@ module.exports = {
       },
     },
   ],
-  packagerConfig: {},
+  packagerConfig: {
+    osxSign: {},
+    osxNotarize: {
+      tool: 'notarytool',
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID,
+    }
+  },
   rebuildConfig: {},
   makers: [
     {
